d1a0d4be96bef944904fa3c2dc66676492eb5675
sammyboi <Samuel_w_Johnson@hotmail.com>
2026-05-14 13:25:10 +1000

    Add files via upload

---
+++ airplanne.js
@@ -0,0 +1,21 @@
+// Save this file exactly as "airplane_mod.js"
+elements.airplane = {
+    color: "#d4d4d4",
+    behavior: [
+        "XX|CR:smoke|XX",   // Spawns smoke above/behind it
+        "XX|XX|M1|M2",      // Moves forward and up diagonally
+        "XX|XX|XX"
+    ],
+    category: "machines",
+    state: "solid",
+    density: 10,
+    hardness: 50
+};
+
+// Optional: Make it drop bombs if it hits something
+elements.airplane.tick = function(pixel) {
+    // If it collides with a structure, transform into an explosion
+    if (neighborsCount(pixel, "solid") > 1) {
+        changePixel(pixel, "explosion");
+    }
+};
